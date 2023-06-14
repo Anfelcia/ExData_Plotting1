2371198ee6b34f3d33b05a18fb7f76c0a7e000f0
Anfelcia <58438292+Anfelcia@users.noreply.github.com>
2023-06-15 00:29:38 +0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,9 @@
+# Define the file path for the data
+dataFile <- "./data/household_power_consumption.txt"
+
+# Read the data from the file into a table
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
 # Subset the data based on specific dates
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007"), ]
 
